6841a214da6b159b081c974f08edf9effcca5e97
calefrey <calefrey@gmail.com>
2018-06-10 16:22:50 -0400

    Accept input from console

--- run.py
+++ run.py
@@ -1,10 +1,17 @@
 from player import player
 from room import room
-
-art = room("Gallery")
-garden = room("Garden")
+from initialize import *
 
 tandrew = player("Tandrew", "Noob")
 caleb = player("Caleeb", "boss")
 
+kitchen.enter(caleb)
 
+def run():
+    resp = input("Where do you want to go? ")
+    for room in caleb.location.adjacent:
+        if resp == room.name:
+            room.enter(caleb)
+    run()
+
+run()
