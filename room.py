216d22e52cefedab486f87333b1f70b246768caa
calefrey <calefrey@gmail.com>
2018-06-10 16:22:26 -0400

    Added nearby room listings and player movement

--- room.py
+++ room.py
@@ -2,14 +2,18 @@ class room:
     def __init__(self, name):
         self.name = name
         self.occupants = []
-    
+
+    def nearby(self, rooms):
+        self.adjacent = rooms   #Accepts an array of rooms
+
     def enter(self, player):
         self.occupants.append(player)
+        player.move(self)
     
     def leave(self, player):
         self.occupants.remove(player)
 
-    def print(self):
-        print(self.name)
+    def list(self):
+        print(self.name+":")
         for i in self.occupants:
             print(i.name)
