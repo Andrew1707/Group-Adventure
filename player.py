4faff383d9ae45b3c2faea231f505fba2c564e2f
calefrey <calefrey@gmail.com>
2018-06-10 16:21:51 -0400

    Added moved method, which includes prompt for nearby rooms

--- player.py
+++ player.py
@@ -2,3 +2,9 @@ class player:
     def __init__(self, name, role):
         self.name = name
         self.role = role
+    def move(self, room):
+        self.location = room
+        print("Moved to "+ room.name)
+        print("Nearby Rooms:")
+        for room in self.location.adjacent:
+            print(room.name)
