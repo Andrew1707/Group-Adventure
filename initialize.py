2878e76b639f9cf7553e23531a7e20fb1bb35d1b
Andrew1707 <36525532+Andrew1707@users.noreply.github.com>
2018-09-07 20:09:17 -0400

    finished list of nearby rooms
    All the rooms now know what rooms are nearby

--- initialize.py
+++ initialize.py
@@ -14,15 +14,16 @@ kitchen = room("Kitchen")
 sunroom = room("Sunroom")
 pantry = room("Pantry")
 bath1 = room("Bathroom")
-stairs2 = room("Stairs between first and second floor")
+stairs2 = room("Down the stairs between first and second floor")
 
 #Second Floor
-bed1 = room("Bedroom")
+bed1 = room("Boy Bedroom")
 master = room("Master Bedroom")
-guest = room("Guest Bedroom")
+bed2 = room("Girl Bedroom")
 library = room("Library")
-bath2 = room("Master Bathroom")
-bath3 = room("Bathroom")
+bath2 = room("Bathroom")
+stairs3 = room("Up the stairs between the first and second floor")
+
 
 #Outside
 maze = room("The Maze")
@@ -30,5 +31,26 @@ garden = room("The Garden")
 shed = room("Shed")
 graveyard = room("Graveyard")
 
+laundry.nearby([cellar,furnace,stairs2])
+furnace.nearby([laundry,butchers])
+butchers.nearby([cellar,furnace])
+cellar.nearby([stairs1,butchers,laundry])
+stairs1.nearby([cellar,dinning])
+
 kitchen.nearby([dining, gallery, pantry])
-gallery.nearby([bath1, kitchen, dining])
+gallery.nearby([kitchen, stairs2])
+pantry.nearby([kitchen])
+dinning.nearby([sunroom,bath1,stairs1])
+bath1.nearby([dinning])
+sunroom.nearby([stairs2,dinning])
+stairs2.nearby([stairs3,sunroom,gallery,laundry])
+
+stairs3.nearby([bed1,bed2,master,bath2,stairs2,library])
+master.nearby([stairs3])
+bed1.nearby([stairs3,])
+bed2.nearby([stairs3])
+library.nearby([stairs3])
+bath2.nearby([stairs3])
+
+
+   
